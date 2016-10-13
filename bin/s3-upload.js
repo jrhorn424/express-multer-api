5082c5bd92560696dacc69316a8acbe57692c13b
Jeffrey Horn <jeffh@generalassemb.ly>
2016-10-13 13:59:12 -0400

    Wrap fs.readFile in a Promise
    Define a wrapper for function for fs.readFile to aid
    readability. Also define a wrapper for the logger, which
    we will change later to do something useful (like upload something).

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -6,10 +6,23 @@ const fs = require('fs');
 
 const filename = process.argv[2] || '';
 
-fs.readFile(filename, (error, data) => {
-  if (error) {
-    return console.error(error);
-  }
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, (error, data) => {
+      if (error) {
+        reject(error);
+      }
 
+      resolve(data);
+    });
+  });
+};
+
+const logMessage = (data) => {
   console.log(`${filename} is ${data.length} bytes long`);
-});
+};
+
+readFile(filename)
+.then(logMessage)
+.catch(console.error)
+;
