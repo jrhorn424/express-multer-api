fb70f8bd51bc350998755f43c800c4bb3a869609
Jeffrey Horn <jeffh@generalassemb.ly>
2016-10-14 10:13:40 -0400

    Implement upload using AWS.S3.upload() method
    -   Use dotenv for secret keys
    -   Use aws-sdk for the upload
    -   Configure AWS.S3 to use our credentials
    -   Do a test upload
    -   Change the log
    
    We need to generate better names for our files. These names should
    be unique with a low chance of duplication, so we'll start with
    a PRNG (the node `crypto` module).

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,7 +1,11 @@
 'use strict';
 
+// this has to come before anything else
+require('dotenv').config();
+
 const fs = require('fs');
 const fileType = require('file-type');
+const AWS = require('aws-sdk');
 
 const filename = process.argv[2] || '';
 
@@ -32,6 +36,13 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  }
+});
+
 const upload = (file) => {
   const options = {
     // get the bucket name from your AWS S3 console
@@ -46,16 +57,20 @@ const upload = (file) => {
     Key: `test/test.${file.ext}`
   };
 
-  // don't actually upload yet, just pass the data down the Promise chain
-  return Promise.resolve(options);
+  return new Promise((resolve, reject) => {
+    s3.upload(options, (error, data) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(data);
+    });
+  });
 };
 
-const logMessage = (upload) => {
-  // get rid of the stream for now, so I can log the rest of my options in the
-  // terminal without seeing the stream
-  delete upload.Body;
+const logMessage = (response) => {
   // turn the pojo into a string so I can see it on the console
-  console.log(`the upload options are ${JSON.stringify(upload)}`);
+  console.log(`the response from AWS was ${JSON.stringify(response)}`);
 };
 
 readFile(filename)
